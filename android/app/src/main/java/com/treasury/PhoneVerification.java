f8838d70cbe1bd482508456a94e2f00d3f24ae25
Uneeb Agha <uneeb.i.am@gmail.com>
2018-05-09 13:13:36 -0700

    Phone verification start

---
+++ android/app/src/main/java/com/treasury/PhoneVerification.java
@@ -0,0 +1,20 @@
+package com.treasury;
+
+
+        import android.widget.Toast;
+
+        import com.facebook.react.bridge.NativeModule;
+        import com.facebook.react.bridge.ReactApplicationContext;
+        import com.facebook.react.bridge.ReactContext;
+        import com.facebook.react.bridge.ReactContextBaseJavaModule;
+        import com.facebook.react.bridge.ReactMethod;
+
+        import java.util.Map;
+        import java.util.HashMap;
+
+public class PhoneVerification extends ReactContextBaseJavaModule {
+
+    public ToastModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+}
